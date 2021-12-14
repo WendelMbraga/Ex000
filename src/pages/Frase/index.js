3034498a2f65956496de8d1899e2574d61176252
unknown <wendel984143238@gmail.com>
2021-12-13 23:24:48 -0400

    first commit

---
+++ src/pages/Frase/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Frase(){
+    return(
+        <h1><i>"Sogeking, senta o dedo naquela bandeira" -Luffy 
+            <p>Anime: One piece</p> </i></h1>
+    );
+};
+
+export default Frase;
